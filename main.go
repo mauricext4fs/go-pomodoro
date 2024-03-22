bf4e9ab17aaf071c623cb5cbac3c752b7820162d
Maurice Courtois <courtoisninja@gmail.com>
2024-03-22 16:32:56 +0100

    Switch to using Stack and added some struct for the UI

--- main.go
+++ main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type clock struct {
+	canvas      fyne.CanvasObject
+	MinuteLabel *widget.Label
+	SecondLabel *widget.Label
+	countdown   countdown
+	stop        bool
+}
+
+type countdown struct {
+	minute      int64
+	second      int64
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Go Pomodoro")
+	c := container.NewStack();
 
+	c.Add(widget.NewLabel("Remaining Time: "))
 
 	w.Resize(fyne.Size{Width: 1024, Height: 768})
-	w.SetContent(widget.NewLabel("Remaining Time: "))
+	w.CenterOnScreen()
+	w.SetContent(c)
 	w.ShowAndRun()
 }
 
