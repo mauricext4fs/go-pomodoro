a7df43f9184a550db16dcb7149e66ca192bb759c
Maurice Courtois <courtoisninja@gmail.com>
2025-03-10 17:57:59 +0100

    WP: Trying to solve crashing on playing the wav for the second time

--- media.go
+++ media.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/gopxl/beep"
@@ -10,8 +12,25 @@ import (
 	"github.com/gopxl/beep/wav"
 )
 
+func createTmpWaveFile(wave []byte) {
+	f, err := os.CreateTemp("", "pomodoro_sound")
+	if err != nil {
+		log.Fatalln("Could not create a temporary file: ", err)
+	}
+
+	fmt.Println("Temp file name: ", f.Name())
+
+	defer os.Remove(f.Name())
+
+	_, err = f.Write(wave)
+	if err != nil {
+		log.Fatalln("Could not write to temporary file: ", f.Name())
+	}
+}
+
 func PlayNotificationSound() {
 	wR := bytes.NewReader(resourceNotificationWav.Content())
+	createTmpWaveFile(resourceNotificationWav.Content())
 	streamer, format, err := wav.Decode(wR)
 	if err != nil {
 		log.Fatal(err)
